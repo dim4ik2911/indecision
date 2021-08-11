d04554610b473f047b7d0767c528da38e1e27844
Dmitrijs Paklons <dim4ik2911@gmail.com>
2021-08-11 23:37:16 +0100

    jsx_expressions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,19 @@
 "use strict";
 
 //JSX - javascript syntax extension
-var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Indecision app"), /*#__PURE__*/React.createElement("p", null, "Some text"), /*#__PURE__*/React.createElement("ol", null, /*#__PURE__*/React.createElement("li", null, "Item one"), /*#__PURE__*/React.createElement("li", null, "Item two")));
-var templateTwo = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Dmitrijs Paklons"), /*#__PURE__*/React.createElement("p", null, "Age: 23"), /*#__PURE__*/React.createElement("p", null, "Location: Rezekne"));
+var app = {
+  title: "Indecision app!",
+  subtitle: "Let's try!"
+};
+var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, app.title), /*#__PURE__*/React.createElement("p", null, app.subtitle), /*#__PURE__*/React.createElement("ol", null, /*#__PURE__*/React.createElement("li", null, "Item one"), /*#__PURE__*/React.createElement("li", null, "Item two")));
+var user = {
+  name: "John",
+  age: 22,
+  location: "London"
+};
+var userName = "Mike";
+var userAge = 27;
+var userLocation = "Boston";
+var templateTwo = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, user.name), /*#__PURE__*/React.createElement("p", null, "Age: ", userAge), /*#__PURE__*/React.createElement("p", null, "Location: ", userLocation));
 var appRoot = document.getElementById("app");
-ReactDOM.render(templateTwo, appRoot);
+ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 //JSX - javascript syntax extension
+var app = {
+  title: "Indecision app!",
+  subtitle: "Let's try!",
+};
 var template = (
   <div>
-    <h1>Indecision app</h1>
-    <p>Some text</p>
+    <h1>{app.title}</h1>
+    <p>{app.subtitle}</p>
     <ol>
       <li>Item one</li>
       <li>Item two</li>
@@ -10,14 +14,22 @@ var template = (
   </div>
 );
 
+var user = {
+  name: "John",
+  age: 22,
+  location: "London",
+};
+var userName = "Mike";
+var userAge = 27;
+var userLocation = "Boston";
 var templateTwo = (
   <div>
-    <h1>Dmitrijs Paklons</h1>
-    <p>Age: 23</p>
-    <p>Location: Rezekne</p>
+    <h1>{user.name}</h1>
+    <p>Age: {userAge}</p>
+    <p>Location: {userLocation}</p>
   </div>
 );
 
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(templateTwo, appRoot);
+ReactDOM.render(template, appRoot);

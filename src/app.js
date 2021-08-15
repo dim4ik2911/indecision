0a197a9f12091e72de3422a778d444658068deee
Dmitrijs Paklons <dim4ik2911@gmail.com>
2021-08-15 23:39:36 +0100

    picking an option

--- src/app.js
+++ src/app.js
@@ -21,6 +21,12 @@ const onRemoveAll = () => {
   render();
 };
 
+const onMakeDecision = () => {
+  const randomNum = Math.floor(Math.random() * app.options.length);
+  const option = app.options[randomNum];
+  alert(option);
+};
+
 const render = () => {
   const template = (
     <div>
@@ -28,6 +34,9 @@ const render = () => {
       {app.subtitle && <p>{app.subtitle}</p>}
       <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
       <p>{app.options.length}</p>
+      <button disabled={app.options.length === 0} onClick={onMakeDecision}>
+        What should I do?
+      </button>
       <button onClick={onRemoveAll}>Remove all</button>
 
       <ol>

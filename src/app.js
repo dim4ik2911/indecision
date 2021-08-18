a5a43e038aaf70dfb9f2625987546a9d5591acb9
Dmitrijs Paklons <dim4ik2911@gmail.com>
2021-08-18 11:12:02 +0100

    using stateless functional components

--- src/app.js
+++ src/app.js
@@ -57,46 +57,37 @@ class IndecisionApp extends React.Component {
     );
   }
 }
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <h2>{props.subtitle}</h2>
+    </div>
+  );
+};
 
-class Header extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>{this.props.title}</h1>
-        <h2>{this.props.subtitle}</h2>
-      </div>
-    );
-  }
-}
+const Action = (props) => {
+  return (
+    <button onClick={props.handlePick} disabled={!props.hasOptions}>
+      What should I do?
+    </button>
+  );
+};
 
-class Action extends React.Component {
-  render() {
-    return (
-      <button onClick={this.props.handlePick} disabled={!this.props.hasOptions}>
-        What should I do?
-      </button>
-    );
-  }
-}
+const Options = (props) => {
+  return (
+    <div>
+      <button onClick={props.handleDeleteOptions}>Remove All</button>
+      {props.options.map((option) => (
+        <Option key={option} optionText={option} />
+      ))}
+    </div>
+  );
+};
 
-class Options extends React.Component {
-  render() {
-    return (
-      <div>
-        <button onClick={this.props.handleDeleteOptions}>Remove All</button>
-        {this.props.options.map((option) => (
-          <Option key={option} optionText={option} />
-        ))}
-      </div>
-    );
-  }
-}
-
-class Option extends React.Component {
-  render() {
-    return <p>Option: {this.props.optionText}</p>;
-  }
-}
+const Option = (props) => {
+  return <p>Option: {props.optionText}</p>;
+};
 
 class AddOption extends React.Component {
   constructor(props) {
@@ -131,5 +122,14 @@ class AddOption extends React.Component {
     );
   }
 }
+
+// const User = (props) => {
+//   return (
+//     <div>
+//       <p>Name: {props.name}</p>
+//       <p>Age: {props.age}</p>
+//     </div>
+//   );
+// };
 
 ReactDOM.render(<IndecisionApp />, document.getElementById("app"));

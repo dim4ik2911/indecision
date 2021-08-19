3d6a385d8b92629ed2689f3c2fd4832bb67dd552
Dmitrijs Paklons <dim4ik2911@gmail.com>
2021-08-19 14:39:27 +0100

    import and export with webpack

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-console.log("app.js is running!!!");
+// // import "./utils";
+// import { square, add } from "./utils";
+
+// console.log("app.js is running!!!");
+// console.log(square(4));
+// console.log(add(2, 4));
+import { isAdult, canDrink } from "./person.js";
+
+console.log(isAdult(22));
+console.log(canDrink(16));

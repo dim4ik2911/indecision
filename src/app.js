9fd85cd8ec178e14e5c269d875effce02537d277
Dmitrijs Paklons <dim4ik2911@gmail.com>
2021-08-19 16:10:55 +0100

    default export practising

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 // // import "./utils";
-// import { square, add } from "./utils";
+// import anythingIwant, { square, add } from "./utils";
 
 // console.log("app.js is running!!!");
 // console.log(square(4));
 // console.log(add(2, 4));
-import { isAdult, canDrink } from "./person.js";
+// console.log(anythingIwant(100, 81));
 
+import isSenior, { isAdult, canDrink } from "./person.js";
+
+console.log(isSenior(66));
 console.log(isAdult(22));
 console.log(canDrink(16));

52c21daad611747885fddc94f02d23f111ad805b
Dmitrijs Paklons <dim4ik2911@gmail.com>
2021-08-21 13:18:21 +0100

    styling react  modal

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -7,10 +7,14 @@ const OptionModal = (props) => (
     contentLabel="Selected Option"
     //on request close person can clisn esc button or on any place on the screen and the modal will close
     onRequestClose={props.handleClearSelectedOption}
+    closeTimeoutMS={200}
+    className="modal"
   >
-    <h3>Selected option</h3>
-    <p>{props.selectedOption}</p>
-    <button onClick={props.handleClearSelectedOption}>Okay</button>
+    <h3 className="modal__title">Selected option</h3>
+    <p className="modal__body">{props.selectedOption}</p>
+    <button className="button" onClick={props.handleClearSelectedOption}>
+      Okay
+    </button>
   </Modal>
 );
 export default OptionModal;
